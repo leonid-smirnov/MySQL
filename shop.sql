6bc3b43da0a35f6c40e51b50ed0bc31dfc265969
leonid-smirnov <84706454+leonid-smirnov@users.noreply.github.com>
2021-09-03 11:44:36 +0300

    lesson2

---
+++ shop.sql
@@ -0,0 +1,79 @@
+DROP TABLE IF EXISTS catalogs;
+CREATE TABLE catalogs(
+id SERIAL PRIMARY KEY,
+name VARCHAR(225) COMMENT 'Название раздела'
+) COMMENT = 'Разделы интернет-магазина';
+
+INSERT INTO catalogs VALUES
+(DEFAULT, 'Процессоры'),
+(DEFAULT, 'Видео карты'),
+(DEFAULT, 'DDR');
+
+SELECT id, name FROM catalogs;
+
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
+id SERIAL PRIMARY KEY,
+name VARCHAR(225) COMMENT 'Имя покупателя',
+created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Покупатели';
+
+DROP TABLE IF EXISTS products;
+CREATE TABLE products (
+id SERIAL PRIMARY KEY,
+name VARCHAR(225) COMMENT 'Название',
+description TEXT COMMENT 'Описание',
+price DECIMAL (11,2) COMMENT 'Цена',
+catalog_id INT UNSIGNED,
+created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+KEY index_of_catalog_id(catalog_id)
+) COMMENT = 'Товарные позиции';
+
+DROP TABLE IF EXISTS orders;
+CREATE TABLE orders(
+id INT UNSIGNED NOT NULL,
+user_id INT UNSIGNED,
+created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Заказы';
+
+DROP TABLE IF EXISTS orders_products;
+CREATE TABLE orders_products (
+id INT UNSIGNED NOT NULL,
+order_id INT UNSIGNED,
+product_id INT UNSIGNED,
+total INT UNSIGNED DEFAULT 1 COMMENT 'Количество заказанных товарных позиций'
+) COMMENT = 'Состав заказа';
+
+DROP TABLE IF EXISTS discounts;
+CREATE TABLE discounts (
+id INT UNSIGNED NOT NULL,
+user_id INT UNSIGNED,
+product_id INT UNSIGNED,
+discount FLOAT UNSIGNED COMMENT 'Величина скидки от 0.0 до 1.0',
+started_at DATETIME,
+finished_at DATETIME,
+created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Скидки';
+
+DROP TABLE IF EXISTS storehouses;
+CREATE TABLE storehouses (
+id INT UNSIGNED NOT NULL,
+name VARCHAR(255) COMMENT 'Название',
+created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Склады';
+
+DROP TABLE IF EXISTS storehouses_products;
+CREATE TABLE storehouses_products (
+id INT UNSIGNED NOT NULL,
+storehouses_id INT UNSIGNED,
+product_id INT UNSIGNED,
+value INT UNSIGNED COMMENT 'Запас товарной позиции на складе',
+created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Запасы на складе';
